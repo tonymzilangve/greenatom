a20d907789573b33475fe8f644b8f23f8310afda
Tonio <toha.futbolist@gmail.com>
2023-02-15 16:59:18 +1000

    5/5

--- task2.py
+++ task2.py
@@ -9,15 +9,14 @@ def create_handlers(callback: Callable[[int], Any]) -> List[Callable[[int], Any]
     handlers = []
     for step in range(5):
         # добавляем обработчики для каждого шага (от 0 до 4)
-        handlers.append(lambda callback: callback(step))
-    print(handlers)
+        handlers.append(lambda step=step: callback(step))
     return handlers
 
 
 def execute_handlers(handlers: List[Callable[[int], Any]]) -> None:
     # запускаем добавленные обработчики(шаги от 0 до 4)
     for handler in handlers:
-        handler()
+        print(handler())
 
 
 if __name__ == '__main__':
